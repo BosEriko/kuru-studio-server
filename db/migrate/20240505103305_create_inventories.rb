f608484a7ad26d5fba29a5d4f2b4d1b92ce89d42
BosEriko <bos@eriko.ph>
2024-07-17 21:23:37 +0800

    add default to inventory type and amount type

--- db/migrate/20240505103305_create_inventories.rb
+++ db/migrate/20240505103305_create_inventories.rb
@@ -2,9 +2,9 @@ class CreateInventories < ActiveRecord::Migration[7.0]
   def change
     create_table :inventories, id: :uuid do |t|
       t.string :name, null: false
-      t.string :inventory_type, null: false
+      t.string :inventory_type, null: false, default: "material"
       t.integer :amount, null: false, default: 0
-      t.string :amount_type, null: false
+      t.string :amount_type, null: false, default: "quantity"
       t.uuid :tenant_id, null: false
 
       t.timestamps
