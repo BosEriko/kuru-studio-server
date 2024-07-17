68b8a539cf6f0475a65d59d840d27199b8059fa3
BosEriko <bos@eriko.ph>
2024-07-17 23:40:58 +0800

    add status type

--- db/schema.rb
+++ db/schema.rb
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_17_151804) do
   end
 
   create_table "orders", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "status_type", default: "order_placed", null: false
     t.uuid "user_id", null: false
     t.uuid "tenant_id", null: false
     t.datetime "created_at", null: false
