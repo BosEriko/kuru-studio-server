888bbdcac391ff1226f8ce8ebe1696c860fe5f3f
BosEriko <bos@eriko.ph>
2024-07-18 21:12:30 +0800

    add variety to inventories

--- db/migrate/20240505103305_create_inventories.rb
+++ db/migrate/20240505103305_create_inventories.rb
@@ -5,6 +5,7 @@ class CreateInventories < ActiveRecord::Migration[7.0]
       t.string :inventory_type, null: false, default: "material"
       t.integer :amount, null: false, default: 0
       t.string :amount_type, null: false, default: "quantity"
+      t.string :variety
       t.string :color
       t.string :size
       t.string :cover_image_url
