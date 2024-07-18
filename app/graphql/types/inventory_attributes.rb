4c5a893a65a844fed9c414083437170aa10f9777
BosEriko <bos@eriko.ph>
2024-07-18 21:54:42 +0800

    create gql attributes/type for inventory

--- app/graphql/types/inventory_attributes.rb
+++ app/graphql/types/inventory_attributes.rb
@@ -2,5 +2,12 @@ module Types
   class InventoryAttributes < Types::BaseInputObject
     argument :name, String, required: true
     argument :inventory_type, String, required: true
+    argument :amount, Integer, required: true
+    argument :amount_type, String, required: true
+    argument :variety, String, required: false
+    argument :color, String, required: false
+    argument :size, String, required: false
+    argument :cover_image_url, String, required: false
+    argument :other_fields, GraphQL::Types::JSON, required: false
   end
 end
--- app/graphql/types/inventory_type.rb
+++ app/graphql/types/inventory_type.rb
@@ -5,6 +5,15 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :inventory_type, String, null: false
+    field :amount, Integer, null: false
+    field :amount_type, String, null: false
+    field :variety, String, null: true
+    field :color, String, null: true
+    field :size, String, null: true
+    field :cover_image_url, String, null: true
+    field :other_fields, GraphQL::Types::JSON, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
 
